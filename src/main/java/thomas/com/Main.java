cb148cec0b253055f12e0dafac423e9478230db0
Thomas Abas <abasienyenethomas2@gmail.com>
2025-11-12 18:00:18 +0100

    first commit

--- src/main/java/thomas/com/Main.java
+++ src/main/java/thomas/com/Main.java
@@ -3,15 +3,36 @@ package thomas.com;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//    public static void main(String[] args) {
+//        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//        // to see how IntelliJ IDEA suggests fixing it.
+//        System.out.printf("Hello and welcome!");
+//
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//            System.out.println("i = " + i);
+//        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+//        Given an array of ints, return true if 6 appears
+//        as either the first or last element in the array. 
+//        The array will be length 1 or more.
+//
+//
+//        firstLast6([1, 2, 6]) → true
+//        firstLast6([6, 1, 2, 3]) → true
+//        firstLast6([13, 6, 1, 2, 3]) → false
+        public boolean firstLast6(int[] nums){
+
+            for (int i=0; i< nums.length; i++){
+                    if (nums[0] == 6 || nums[nums.length-1] ==6 ){
+                        return true;
+                    }else {
+                        return false;
+                    }
+            }
+        return true;
         }
-    }
+
 }
